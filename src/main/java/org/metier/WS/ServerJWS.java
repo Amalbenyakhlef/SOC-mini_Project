08c7396ee82bee4d727350d396f101543157973e
Amalbenyakhlef <amel.benyakhlef@gmail.com>
2023-10-27 09:19:51 +0100

    first step

---
+++ src/main/java/org/metier/WS/ServerJWS.java
@@ -0,0 +1,15 @@
+package org.metier.WS;
+
+import jakarta.xml.ws.Endpoint;
+import org.metier.banqueService;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String url="http://0.0.0.0:9000/";
+        Endpoint.publish(url,new banqueService());
+        System.out.println("web service deploy sur "+ url);
+
+
+
+}
